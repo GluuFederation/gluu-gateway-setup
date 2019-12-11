9e38a3e98c3a12e0b4563cf0c3bfd995cfcc6e71
Alexander Altshuler <altexy@gmail.com>
2019-12-11 12:59:42 +0300

    remove not related folders after git repo forking

--- kong/plugins/gluu-metrics/api.lua
+++
@@ -1,9 +0,0 @@
-local metrics = require "kong.plugins.gluu-metrics.metrics"
-
-return {
-  ["/gluu-metrics"] = {
-    GET = function(self, dao_factory)
-        return metrics.collect()
-    end,
-  },
-}
