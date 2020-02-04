aa977b4a1d8ddfe6a31d91e65197b1a7c308c1e3
Meghna Joshi <ldeveloperl1985@gmail.com>
2020-02-04 15:38:55 +0530

    Updated Kong version #7

--- setup/setup-gluu-gateway.py
+++ setup/setup-gluu-gateway.py
@@ -164,10 +164,10 @@ class KongSetup(object):
         self.oxd_archived_log_filename_pattern = "/var/log/oxd-server/oxd-server-%d{yyyy-MM-dd}-%i.log.gz"
 
         # kong package file names
-        self.ubuntu16_kong_file = "kong-1.5.0.xenial.amd64.deb"
-        self.centos7_kong_file = "kong-1.5.0.el7.amd64.rpm"
-        self.rhel7_kong_file = "kong-1.5.0.rhel7.amd64.rpm"
-        self.ubuntu18_kong_file = "kong-1.5.0.bionic.amd64.deb"
+        self.ubuntu16_kong_file = "kong-2.0.0.xenial.amd64.deb"
+        self.centos7_kong_file = "kong-2.0.0.el7.amd64.rpm"
+        self.rhel7_kong_file = "kong-2.0.0.rhel7.amd64.rpm"
+        self.ubuntu18_kong_file = "kong-2.0.0.bionic.amd64.deb"
 
     def init_parameters_from_json_argument(self):
         if len(sys.argv) > 1:
