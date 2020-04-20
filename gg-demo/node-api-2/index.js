da6f7d34d92676a677c16d4dc4ecda2ab7971945
ldeveloperl1985 <ldeveloperl1985@gmail.com>
2020-04-20 19:09:31 +0530

    feat: Updated gg demo node api 2

--- gg-demo/node-api-2/index.js
+++ gg-demo/node-api-2/index.js
@@ -2,47 +2,16 @@ var express = require("express");
 var app = express();
 
 
-var modules = ['/item', '/company']
+var modules = ['/items', '/companies', '/posts', '/images', '/users', '/heros', '/todos']
 
-modules.forEach(function (module) {
-  app.get(module, function (req, res) {
-    res.json([
-      {
-        "id": 1,
-        "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-        "body": "quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto"
-      },
-      {
-        "id": 2,
-        "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-        "body": "quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto"
-      }]);
-  });
 
-  app.put(module + "/:id", function (req, res) {
+app.get("/*", function (req, res) {
     res.json({
       "id": 1,
       "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
       "body": "quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto"
     });
   });
-
-  app.post(module, function (req, res) {
-    res.json({
-      "id": 1,
-      "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-      "body": "quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto"
-    });
-  });
-
-  app.delete(module + "/:id", function (req, res) {
-    res.json({
-      "id": 1,
-      "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-      "body": "quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto"
-    });
-  });
-});
 
 
 app.listen(3000, function () {
