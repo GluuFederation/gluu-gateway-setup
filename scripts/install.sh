5827083d52c14f9bf46722a2816a305365469890
duttarnab <32794267+duttarnab@users.noreply.github.com>
2020-08-21 16:33:51 +0530

    #15 - Adding postgres script for Centos 7

--- scripts/install.sh
+++ scripts/install.sh
@@ -47,6 +47,7 @@ function prepareSourcesCentos7 {
     wget https://repo.gluu.org/centos/RPM-GPG-KEY-GLUU -O /etc/pki/rpm-gpg/RPM-GPG-KEY-GLUU
     rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-GLUU
     rpm -Uvh https://yum.postgresql.org/10/redhat/rhel-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm
+    rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
     curl -sL https://rpm.nodesource.com/setup_10.x | sudo -E bash -
 }
 
